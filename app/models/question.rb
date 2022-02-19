5ea62e487270ef5b6dafd3a244ff9e657e3e72d7
melhemm <jamalmulhemm@gmail.com>
2022-02-19 20:49:31 +0300

    active storage

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,8 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+
+  has_many_attached :files
+
   belongs_to :user, class_name: 'User'
 
   validates :title, :body, presence: true
