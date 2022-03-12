06c0c43ef68f9d744d20154fe1c26886ec3c4508
melhemm <jamalmulhemm@gmail.com>
2022-03-12 21:13:43 +0300

    oauth tests and fixes

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -7,4 +7,16 @@ module ControllerHelpers
   def add_file_to(model)
     model.files.attach(io: File.open("#{Rails.root}/spec/rails_helper.rb"), filename: 'rails_helper.rb')
   end
+
+  def mock_auth(provider, email: nil)
+    OmniAuth.config.mock_auth[provider] = OmniAuth::AuthHash.new(
+      provider: provider.to_s,
+      uid: '123456789',
+      info: {
+        name: 'UserName',
+        email: email
+      }
+    )
+  end
+
 end
