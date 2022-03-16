8d8bb8959e6dead9a580bea2c265341acf05f50a
melhemm <jamalmulhemm@gmail.com>
2022-03-16 14:56:49 +0300

    done some improvements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to root_url, alert: exception.message }
+      format.js { head :forbidden }
+    end
   end
 
   check_authorization unless: :devise_controller?
