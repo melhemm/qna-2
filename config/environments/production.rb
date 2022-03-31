68733ad016915e02682a30bdb6bb66936bfb8dd3
melhemm <jamalmulhemm@gmail.com>
2022-04-01 01:05:42 +0300

    fix err for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            Rails.application.credentials.dig(:google_smtp, :email),
-    password:             Rails.application.credentials.dig(:google_smtp, :password),
+    user_name:            Rails.application.credentials[Rails.env.to_sym][:google_smtp][:email],
+    password:             Rails.application.credentials[Rails.env.to_sym][:google_smtp][:password],
     authentication:       'plain',
     enable_starttls_auto: true 
   }
