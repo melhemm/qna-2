abcf6360474f1099dcbc61fb41df2bd58e9e8f04
melhemm <jamalmulhemm@gmail.com>
2022-03-31 23:23:20 +0300

    test 2 for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
+    user_name:            Rails.application.credentials.smtp[:SMTP_USERNAME],
+    password:             Rails.application.credentials.smtp[:SMTP_PASSWORD],
     authentication:       'plain',
     enable_starttls_auto: true 
   }
